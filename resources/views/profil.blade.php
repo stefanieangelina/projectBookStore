f3962c48246c1749b61378392a5e2f9b2450314a
stefanie <stefanieangelina.sa@gmail.com>
2020-12-01 20:52:51 +0700

    profile

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -6,16 +6,19 @@
 
 @section('content')
     Nama : {{ Auth::user()->name }}
-    <br/>
+    <br/><br/>
 
     Email : {{ Auth::user()->email }}
-    <br/>
+    <br/><br/>
 
-    Alamat : {{ Auth::user()->address }}
-    <br/>
+    Alamat : {{ (Auth::user()->address == null)? 'Belum ada data' :  Auth::user()->address }}
+    <br/><br/>
 
-    Nomer Telepon : {{ Auth::user()->phone }}
-    <br/>
+    Nomer Telepon : {{ (Auth::user()->phone == null)? 'Belum ada data' :  Auth::user()->phone }}
+    <br/><br/>
+
+    Points : {{ Auth::user()->points }}
+    <br/><br/>
 
     <form method="POST">
         @csrf
