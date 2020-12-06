8d8034727642129c91faf65171d3adbb2fcd6f49
stefanie <stefanieangelina.sa@gmail.com>
2020-12-06 23:26:16 +0700

    benerin tampilan profil

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -5,23 +5,29 @@
 @endsection
 
 @section('content')
-    Nama : {{ Auth::user()->name }}
-    <br/><br/>
+    <div class="container">
+        Nama : {{ Auth::user()->name }}
+        <br/><br/>
 
-    Email : {{ Auth::user()->email }}
-    <br/><br/>
+        Email : {{ Auth::user()->email }}
+        <br/><br/>
 
-    Address : {{ (Auth::user()->address == null)? 'Belum ada data' :  Auth::user()->address }}
-    <br/><br/>
+        Address : {{ (Auth::user()->address == null)? 'Belum ada data' :  Auth::user()->address }}
+        <br/><br/>
 
-    Phone : {{ (Auth::user()->phone == null)? 'Belum ada data' :  Auth::user()->phone }}
-    <br/><br/>
+        Phone : {{ (Auth::user()->phone == null)? 'Belum ada data' :  Auth::user()->phone }}
+        <br/><br/>
 
-    Points : {{ Auth::user()->points }}
-    <br/><br/>
+        Points : {{ Auth::user()->points }}
+        <br/><br/>
 
-    <form method="POST">
-        @csrf
-        <button class="btn btn-primary" name="btnEdit" formaction="/edit">Edit Profile</button>
-    </form>
+        <div class="container2">
+            <form method="POST">
+                @csrf
+                <button class="btn btn-primary" name="btnEdit" formaction="/edit">Edit Profile</button>
+            </form>
+        </div>
+    </div>
+
+
 @endsection
