d2660ca2a7f19326559c5ec2d7e30dfb2e7d0464
stefanie <stefanieangelina.sa@gmail.com>
2020-12-14 11:45:49 +0700

    point

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -284,4 +284,71 @@ class CartController extends Controller
     //dd(Session::get('listTrans'));
         return view('home');
     }
+
+    public function pointPayment(Request $request){
+        $userPoint = Auth::user()->points;
+        $userPoint -= 10;
+        $id = Auth::user()->id;
+
+        $user = User::findorFail($id);
+        $user->points = $userPoint;
+        $user->save();
+
+        foreach (Session::get('listTrans') as $books) {
+            Cart::where('id', $books->id)
+                ->delete();
+        }
+        $newtrans = new htrans();
+        $newtrans->cara_pembayaran = "poin";
+        $newtrans->total = Session::get('total');
+        $newtrans->user_id=Auth::user()->id;
+        $newtrans->status = 0;
+        $newtrans->file_bukti = "";
+        $newtrans->save();
+        $htransID=   DB::table('htrans')
+            ->select('id')
+            ->where('user_id',Auth::user()->id)
+            ->where('total',Session::get('total'))
+            ->first();
+        $userPoint =DB::table('users')
+            ->select('points')
+            ->where('id',Auth::user()->id)
+            ->first();
+        $calcpoint = intval(floor(Session::get('total') /100000));
+        foreach (Session::get('listTrans') as $books) {
+            $dtrans = new dtrans();
+            $dtrans->htrans_id = $htransID->id;
+            $dtrans->books_id =$books->book_id;
+            $dtrans->banyak = $books->qty;
+            $temp = $books->sell_price - $books->discount;
+            $dtrans->satuan = $temp;
+            $dtrans->jumlah = $temp * $books->qty;
+            $dtrans->save();
+            $stok = $books->stock - $books->qty;
+            Books::where('id', $books->book_id)
+                ->update(['stock' => $stok]);
+        }
+        if  (Session::get('total')>=500000){
+            $member =DB::table('users')
+                ->select('isMember')
+                ->where('id',Auth::user()->id)
+                ->first();
+            if($member->isMember =="1"){
+                Users::where('id', Auth::user()->id)
+                    ->update(['points' => $userPoint->points + $calcpoint]);
+            }else if($member->isMember =="0"){
+                Users::where('id', Auth::user()->id)
+                    ->update(['isMember' => 1]);
+                $newVip = new Vip();
+                $newVip->status =1;
+                $newVip->user_id = Auth::user()->id;
+                $newVip->save();
+                Users::where('id', Auth::user()->id)
+                    ->update(['points' => $userPoint->points + $calcpoint]);
+            }
+        }
+
+        return redirect('home')
+            ->with("success", "Succsess pay with point!");
+    }
 }
